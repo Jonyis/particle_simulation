9f96b7da9dae8130f7f3f54ed30a17796f1e61a4
joão silva <joao.s.silva99@gmail.com>
2024-03-23 02:53:40 +0000

    Renderer: add method to clear all particles from the screen

--- Renderer.h
+++ Renderer.h
@@ -10,6 +10,8 @@ public:
 
 	explicit Renderer(sf::RenderWindow& window);
 	
+	void clear();
+
 	void drawAll(const IShape& boundingShape, const std::vector<std::unique_ptr<Particle>>& particles);
 
 	void drawParticles(const std::vector<std::unique_ptr<Particle>>& particles);
