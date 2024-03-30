33f03169f397934de95bab55e1fc8f24e8c1b784
joão silva <joao.s.silva99@gmail.com>
2024-03-30 16:03:01 +0000

    Rectangle: update to make possible rectangles not in origin

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,6 +1,7 @@
 #include "Rectangle.h"
 
-Rectangle::Rectangle(float width, float height) : width(width), height(height), shape({ width, height }) {
+Rectangle::Rectangle(float width, float height, sf::Vector2f center) : width(width), height(height), center(center), shape({width, height}) {
+	shape.setPosition(center.x - width / 2, center.y - height / 2);
 	shape.setFillColor(sf::Color::Transparent);
 	shape.setOutlineThickness(2);
 	shape.setOutlineColor(sf::Color::Red);
@@ -9,13 +10,18 @@ Rectangle::Rectangle(float width, float height) : width(width), height(height), 
 void Rectangle::bounceParticle(Particle& particle) {
 	sf::Vector2f pos = particle.getPosition();
 	float radius = particle.getRadius();
-	if (pos.x + radius > width || pos.x - radius < 0) {
+	static const float leftBound = center.x - width / 2;
+	static const float rightBound = center.x + width / 2;
+	static const float topBound = center.y - height / 2;
+	static const float bottomBound = center.y + height / 2;
+
+	if (pos.x + radius > rightBound || pos.x - radius < leftBound) {
 		sf::Vector2f vel = particle.getVelocity();
 		sf::Vector2f oldPos = pos - vel;
 		bounce(particle, { oldPos.x, pos.y }, { -vel.x, vel.y });
 	}
 
-	if (pos.y + radius > height || pos.y - radius < 0) {
+	if (pos.y + radius > bottomBound || pos.y - radius < topBound) {
 		sf::Vector2f vel = particle.getVelocity();
 		sf::Vector2f oldPos = pos - vel;
 		bounce(particle, { pos.x, oldPos.y }, { vel.x, -vel.y });
@@ -34,7 +40,8 @@ void Rectangle::draw(sf::RenderWindow& window) const {
 }	
 
 sf::Vector2f Rectangle::getRandomPosition() const {
-	auto x = static_cast<float>(rand() % static_cast<int>(width*0.7)) + width * 0.05f;
-	auto y = static_cast<float>(rand() % static_cast<int>(height*0.7)) + height * 0.05f;
+	float padding = 50.f;
+	float x = center.x - width / 2 + static_cast<float>(rand()) / RAND_MAX * (width - padding) + padding/2;
+	float y = center.y - height / 2 + static_cast<float>(rand()) / RAND_MAX * (height - padding) + padding/2;
 	return { x, y };
 }
